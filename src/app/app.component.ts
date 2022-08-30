1d192c355926ee86fa4462713091d0165716cfce
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-08-30 12:53:38 +0300

    Onboarding updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Consult';
+  title = 'Medical_APP';
 
   // tslint:disable-next-line: typedef
   ngOnInit() {}
