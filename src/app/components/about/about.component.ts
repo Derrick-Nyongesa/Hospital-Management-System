7403343592a5be298baa1f1b3b27eb3d5aeb4ab6
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-09-07 13:11:26 +0300

    about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,93 @@
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Profile } from 'src/app/profile';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  profile: Profile;
+  currentId: string;
+  selected!: Date | null;
+  user: Observable<any>;
+  userEmail: any;
 
-  constructor() { }
+  name: any;
+  username: any;
+  uid: any;
 
-  ngOnInit(): void {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(
+    private ngxService: NgxUiLoaderService,
+    private breakpointObserver: BreakpointObserver,
+    private auth: AuthService,
+    private profileService: ProfileService
+  ) {
+    firebase
+      .database()
+      .ref('users/')
+      .on('value', (snapshot: any) => {
+        snapshot.forEach((childSnapshot: any) => {
+          const childKey = childSnapshot.key;
+          const childData = childSnapshot.val();
+          this.username = childData.displayName;
+          this.uid = childKey;
+          console.log(this.username);
+        });
+      });
+    this.findProfiles();
+    this.auth.user.subscribe(
+      (user) => {
+        this.currentId = user.uid;
+        console.log(this.currentId);
+        this.profileService.fetchProfileApi(this.currentId).subscribe(
+          (res) => {
+            this.profile = res;
+            console.log(res);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
+  findProfiles() {}
+
+  ngOnInit(): void {
+    this.ngxService.start(); // start foreground spinner of the master loader with 'default' taskId
+    // Stop the foreground loading after 5s
+    setTimeout(() => {
+      this.ngxService.stop(); // stop foreground spinner of the master loader with 'default' taskId
+    }, 2000);
+
+    this.user.subscribe((user) => {
+      if (user) {
+        this.userEmail = user.email;
+        console.log(user);
+      }
+    });
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 }
