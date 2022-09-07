75b36382673285cb220be0e3d0f107202f7f572c
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-09-07 15:27:30 +0300

    tasks component updated

--- src/app/components/tasks/personal-tasks/tasks-home/tasks-home.component.ts
+++ src/app/components/tasks/personal-tasks/tasks-home/tasks-home.component.ts
@@ -3,13 +3,11 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 import { ProfileService } from 'src/app/services/profile.service';
 import { Profile } from 'src/app/profile';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { CurrentUser } from '../tasks/tasks.component';
-
-
 
 @Component({
   selector: 'app-tasks-home',
@@ -26,16 +24,20 @@ export class TasksHomeComponent implements OnInit {
       map((result) => result.matches),
       shareReplay()
     );
-    user: CurrentUser;
-    
+  user: CurrentUser;
+
+  activeTab = 0;
+
   constructor(
-    private breakpointObserver: BreakpointObserver) {
-        
-        
-   }
+    private breakpointObserver: BreakpointObserver,
+    private ngxService: NgxUiLoaderService
+  ) {}
 
   ngOnInit(): void {
+    this.ngxService.start(); // start foreground spinner of the master loader with 'default' taskId
+    // Stop the foreground loading after 5s
+    setTimeout(() => {
+      this.ngxService.stop(); // stop foreground spinner of the master loader with 'default' taskId
+    }, 2000);
   }
-
-  
 }
