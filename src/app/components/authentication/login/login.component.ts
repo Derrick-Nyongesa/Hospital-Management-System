ecfa489084891da667d237fad8613e5294a02b71
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-12-08 09:33:39 +0300

    login component

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth/auth.service';
 import { MatCarousel, MatCarouselComponent } from 'ng-mat-carousel';
 import { ToastrService } from 'ngx-toastr';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-login',
@@ -12,17 +11,10 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
-    private toastr: ToastrService,
-    private ngxService: NgxUiLoaderService
+    private toastr: ToastrService
   ) {}
 
-  ngOnInit(): void {
-    this.ngxService.start(); // start foreground spinner of the master loader with 'default' taskId
-    // Stop the foreground loading after 5s
-    setTimeout(() => {
-      this.ngxService.stop(); // stop foreground spinner of the master loader with 'default' taskId
-    }, 2000);
-  }
+  ngOnInit(): void {}
 
   // tslint:disable-next-line: typedef
   loginGoogle() {
